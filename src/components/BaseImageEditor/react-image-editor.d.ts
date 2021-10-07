441ecd1de6c1b297ce0e6a847232389932b42bd2
Tommy <tomojung@gmail.com>
2021-10-07 17:13:32 +0700

    test button

--- src/components/BaseImageEditor/react-image-editor.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@toast-ui/react-image-editor' {
-    import ImageEditor from 'tui-image-editor';
-
-    export type ImageEditorProps = ConstructorParameters<typeof ImageEditor>[1];
-
-    export default function ImageEditor(props: ImageEditorProps): JSX.Element;
-}
