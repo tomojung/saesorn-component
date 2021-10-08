4891eb4f653bf0ec7fe7c9fad7644959a9cab959
Tommy <tomojung@gmail.com>
2021-10-08 23:56:42 +0700

    remove image manipulate

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import {Button} from "./components/Button";
-import {BaseImageEditor} from "./components/BaseImageEditor";
 import CustomButton from "./components/CustomButton";
 
 
 export {
     Button,
-    BaseImageEditor,
     CustomButton
 };
